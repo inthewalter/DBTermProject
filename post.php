79ac71b1a4e25ce1fa70d972290f6da3283c02cf
inthewalter <21400564@handong.edu>
2018-05-26 16:43:57 +0900

    comment 구현

--- post.php
+++ post.php
@@ -49,36 +49,9 @@
 								?>
               </div>
               <div class="mdl-card__actions mdl-card--border">
-            <div class="mdl-color-text--primary-contrast mdl-card__supporting-text comments">
-
-              <div class="comment mdl-color-text--grey-700">
-                <header class="comment__header">
-                  <div class="comment__author">
-                    <strong>James Splayd</strong>
-                    <span>2 days ago</span>
-                  </div>
-                </header>
-                <div class="comment__text">
-                  In in culpa nulla elit esse. Ex cillum enim aliquip sit sit ullamco ex eiusmod fugiat. Cupidatat ad minim officia mollit laborum magna dolor tempor cupidatat mollit. Est velit sit ad aliqua ullamco laborum excepteur dolore proident incididunt in labore elit.
-                </div>
-              </div>
-
-              <form action="comment_update.php" method="post" class="comment-form">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-									<input class="mdl-textfield__input" type="text" id="writer">
-								  <label class="mdl-textfield__label" for="writer">comment writer</label>
-							  </div>
-								<br>
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                  <textarea rows=10 class="mdl-textfield__input" id="content"></textarea>
-                  <label for="content" class="mdl-textfield__label">comment content</label>
-                </div>
-								<br>
-                <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-                  <i class="material-icons" role="presentation">check</i>
-                </button>
-              </form>
-            </div>
+							<?php
+								include("./comment.php") ;
+							?>
           </div>
         </div>
 			</div>
